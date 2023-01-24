ad2e0fa12aa7343d4da4f8a91c0b1c2f25cdd869
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 19:19:26 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-// the response object:
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-
-// set-up fectch request
-
 let url = "https://jsonplaceholder.typicode.com/todos/1";
-// fetch(url).then((res) => {
-//   console.log(res.body, "here is the stream");
-//   console.log(res.headers.get("content-type"));
-//   console.log(Array.from(res.headers));
-//   console.log(res.json());
-// }); // note that the response --> is an object (very important in AJAX)
 
 fetch(url)
   .then((res) => res.json()) // json() method is given to us by the browser (is asynchronous / returns a promise)
