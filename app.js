1c1c1056c61917096782012dafca844f3fd24ff9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 19:38:29 -0500

    working with response headers

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function dataToHTML(data) {
   const body = document.getElementById("wrapper");
   body.innerHTML = `<h1>${title}</h1>`;
 }
+
+// all API endpoints will have their own rules (may need to pass in headers to the fetch API)
+
+let dadJokeURL = "https://icanhazdadjoke.com/";
+
+const dadJokeHeaders = {
+  Accept: "application/json",
+}; // this header is needed as specified in the documentation
+
+fetch(dadJokeURL, { headers: dadJokeHeaders })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    return data;
+  })
+  .then(({ joke }) => console.log(joke));
