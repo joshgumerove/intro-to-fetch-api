25619eb443aae3911d9658831295e6cbcfd78f23
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 19:18:26 -0500

    refactor code

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ let url = "https://jsonplaceholder.typicode.com/todos/1";
 
 fetch(url)
   .then((res) => res.json()) // json() method is given to us by the browser (is asynchronous / returns a promise)
-  .then((userData) => {
-    let title = userData.title;
-    let doc = document.getElementById("wrapper");
-    doc.innerHTML = `<h1>${title}</h1>`;
+  .then((data) => {
+    return dataToHTML(data);
   });
+
+function dataToHTML(data) {
+  const title = data.title;
+  const body = document.getElementById("wrapper");
+  body.innerHTML = `<h1>${title}</h1>`;
+}
